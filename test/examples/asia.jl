ad745f0c8baed87b5391679c190d5a01876ae013
dd <uagui@student.kit.edu>
2015-08-30 19:37:01 +0200

    move examples to example folder

---
+++ test/examples/asia.jl
@@ -0,0 +1,8 @@
+# From Pearl 1988, Probabilistic Reasoning in Intelligent Systems
+
+f1 = Factor(["a"], [0.99, 0.01])
+f2 = Factor(["s"], [0.5, 0.5])
+#f3 = Factor(["t","a"])
+
+#IDK, look at http://www.cc.gatech.edu/~dellaert/FrankDellaert/Frank_Dellaert/Entries/2013/5/10_Factor_Graphs_Tutorial_files/2013-05-10-ICRA-small.pdf
+#page 17
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,16 @@
 tests = ["factor_test", "pgm_test"]
 
+examples = ["misconception"]
+
 for t in tests
     fpath = "$t.jl"
     println("running $fpath ...")
     include(fpath)
+end
+
+for e in examples
+  fpath = "examples/$e.jl"
+  println("running $fpath ...")
+  include(fpath)
+  println("Successfully ran $fpath")
 end
