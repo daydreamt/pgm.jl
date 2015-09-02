6bbce9881f0c663be475ea1a0bdb90693de997cc
dd <uagui@student.kit.edu>
2015-09-02 22:17:16 +0200

    Three comments!

--- src/pgm.jl
+++ src/pgm.jl
@@ -2,8 +2,11 @@ module pgm
 
 #This would be a prime candidate for decoupling
 include("factor.jl")
+# Representation
 export Domain, Variable, AbstractFactor, Factor, DiscreteFactor
+# Algorithms
 export compute_Z_brute_force, normalize
+# Wrapped distributions
 export supported_distributions
 # Probably together with that one
 include("factorgraph.jl")
